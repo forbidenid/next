307ba059181ddacb597b4bdee042220487072c48
ctrl-root <muriithidennis340@gamil.com>
2023-04-15 22:21:57 +0300

    code commit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,14 +17,17 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.shortcuts import redirect
+    
 
 
 admin.site.site_header = "ECOMMERCE"
-admin.site.site_title = "ARK ADMIN"
+admin.site.site_title = "NEXT"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('ecomapp.urls')),
+    path('favicon.ico', lambda _ : redirect("static/images/favicon.ico", permanent=True)),
 
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
